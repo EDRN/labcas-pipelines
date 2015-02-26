b61fa3f889b985546c2542d5eb009cf90a50782e
Luca Cinquini <luca.cinquini@jpl.nasa.gov>
2015-02-26 12:41:51 -0700

    Fixing num_nodes in python script.

--- backend/win/config/src/main/conf/biomarker_discovery/Submit1.py
+++ backend/win/config/src/main/conf/biomarker_discovery/Submit1.py
@@ -15,7 +15,7 @@ print 'Project Directory=%s' % projectDirectory
 print 'PROTEOME_HOME=%s' % proteomeHome
 
 # edrn-node0n
-NUM_NODES = 18
+NUM_NODES = 8
 PROTEOME_WORKFLOW_URLS = [ 'http://edrn-node%s:9001' % format(i,'02d') for i in range(1,NUM_NODES+1) ]
 
 
@@ -24,7 +24,7 @@ for i in range(1,ncv+1):
         # choose processing node URL through round-robin
         j = i-1
         if j >= len(PROTEOME_WORKFLOW_URLS):
-	   j = 0
+	    j = 0
         wkfmgrUrl = PROTEOME_WORKFLOW_URLS[j]
 
         command = "cd %s/proteome-workflow-linux/bin; ./wmgr-client --url %s --operation --sendEvent --eventName biomarker-discovery-2 --metaData --key ncv %s" % (proteomeHome, wkfmgrUrl, i)
