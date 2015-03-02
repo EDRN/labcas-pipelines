830c2dbda9e2bc8300d4fe28cb7908130a190fb9
Luca Cinquini <luca.cinquini@jpl.nasa.gov>
2015-03-02 12:35:42 -0700

    Enabling pipeline with 2 products.

--- backend/workflows/proteome-misc-tasks/src/main/scripts/cystfluid-launch.sh
+++ backend/workflows/proteome-misc-tasks/src/main/scripts/cystfluid-launch.sh
@@ -8,10 +8,9 @@
 --metaData \
 --key RawDatasetName 20140506-Pancreatic-Cyst-Fluid \
 --key PrimaryAnalysisDatasetName 0140506-Pancreatic-Cyst-Fluid-Processed \
---key RAWFiles f3f8255c-e270-4303-a69d-1d01b0c98f50 \
+--key RAWFiles f3f8255c-e270-4303-a69d-1d01b0c98f50 e3e8eebb-e885-4cd5-b79e-5ebdf32eeafa \
 --key DBFile /usr/local/edrn/proteome/data/cystfluid/protein.db.fasta \
 --key CntmsFile /usr/local/edrn/proteome/data/cystfluid/Cntms.fasta \
 --key MyriMatchCfgFile /usr/local/edrn/proteome/data/cystfluid/cystfluid.myrimatch.cfg \
 --key AssembleFileList /usr/local/edrn/proteome/data/cystfluid/Assembly.txt \
---key ProductName A1_frx01 \
 --key CopyFiles false --key UserEmail luca.cinquini@jpl.nasa.gov
