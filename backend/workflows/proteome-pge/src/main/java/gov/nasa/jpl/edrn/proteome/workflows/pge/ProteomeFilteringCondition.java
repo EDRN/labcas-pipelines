5a47a7ad981432ae4bc8d3e2f3b87bbdc78194ae
Luca Cinquini <luca.cinquini@jpl.nasa.gov>
2015-03-08 05:27:01 -0600

    Working on proteome precondition.

--- backend/workflows/proteome-pge/src/main/java/gov/nasa/jpl/edrn/proteome/workflows/pge/ProteomeFilteringCondition.java
+++ backend/workflows/proteome-pge/src/main/java/gov/nasa/jpl/edrn/proteome/workflows/pge/ProteomeFilteringCondition.java
@@ -1,5 +1,6 @@
 package gov.nasa.jpl.edrn.proteome.workflows.pge;
 
+import java.io.File;
 import java.util.List;
 import java.util.logging.Logger;
 
@@ -7,49 +8,38 @@ import org.apache.oodt.cas.metadata.Metadata;
 import org.apache.oodt.cas.workflow.structs.WorkflowConditionConfiguration;
 import org.apache.oodt.cas.workflow.structs.WorkflowConditionInstance;
 
+/**
+ * Workflow condition that checks for the existence of output from the Myrimatch task
+ * before staring the idpQonvert task.
+ * Specifically, all needed files A1_frx02-MM.pepXML, A1_frx01-MM.pepXML
+ * must exist in the DATA_STAGING directory.
+ * 
+ * @author Luca Cinquini
+ *
+ */
 public class ProteomeFilteringCondition implements WorkflowConditionInstance {
 	
 	private static final Logger LOG = Logger.getLogger(ProteomeFilteringCondition.class.getName());
 
 	@Override
 	public boolean evaluate(Metadata metadata, WorkflowConditionConfiguration config) {
-		
-		for (String key : metadata.getAllKeys()) {
-			LOG.info("==> key: ["+key+"]     value: ["+metadata.getMetadata(key)+"]");
-		}
-		
-		// retrieve job-level metadata
-		List<String> rawFiles = metadata.getAllMetadata("RAWFiles");
-		for (String rawFile : rawFiles) {
-			LOG.info("Raw file="+rawFile);
-		}
+				
+		// output directory == DATA_STAGING
+		String outputDirectory = config.getProperty("outputDirectory");
+		LOG.info("Searcging outputDirectory="+outputDirectory + " for .pepXML files");
+
+		// loop over expected outpput files from Myrimatch task
 		List<String> rawFileNames = metadata.getAllMetadata("RAWFileNames");
 		for (String rawFileName : rawFileNames) {
-			LOG.info("Raw file name="+rawFileName);
+			String outputFileName = outputDirectory + rawFileName.replace(".raw", "-MM.pepXML"); 
+			File outputFile = new File(outputDirectory, outputFileName);
+			LOG.info("Checking for outputFile"+outputFile.getAbsolutePath());
+			if (!outputFile.exists()) return false;
 		}
 		
-		// retrieve condition configuration
-		String outputDirectory = config.getProperty("outputDirectory");
-		LOG.info("outputDirectory="+outputDirectory);
-
-		// loop over expected output files
-//		Long now = System.currentTimeMillis();
-//		SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
-//		for (int i=1; i<=ncv; i++) {
-//			
-//			String filepath = projectDirectory+"/results/cv/"+prefix+"_iter_"+i+".txt";
-//			File file = new File(filepath);
-//			Long fileLastModified = file.lastModified();
-//			
-//			LOG.info("Checking file: "+filepath+" last modified at:"+sdf.format(fileLastModified));
-//			
-//			// check existence
-//			if (!file.exists()) return false;
-//						
-//		}
-		
 		// all files exist
-		return false;
+		return true;
+		
 	}
 
 }
