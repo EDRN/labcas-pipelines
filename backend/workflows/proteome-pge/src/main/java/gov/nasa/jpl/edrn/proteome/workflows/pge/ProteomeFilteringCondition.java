3dde7e0a01b8b56fa1c66c5c205a22ffaa6f439a
Luca Cinquini <luca.cinquini@jpl.nasa.gov>
2015-03-08 04:54:56 -0600

    Worjing on proteome precondition.

---
+++ backend/workflows/proteome-pge/src/main/java/gov/nasa/jpl/edrn/proteome/workflows/pge/ProteomeFilteringCondition.java
@@ -0,0 +1,51 @@
+package gov.nasa.jpl.edrn.proteome.workflows.pge;
+
+import java.util.List;
+import java.util.logging.Logger;
+
+import org.apache.oodt.cas.metadata.Metadata;
+import org.apache.oodt.cas.workflow.structs.WorkflowConditionConfiguration;
+import org.apache.oodt.cas.workflow.structs.WorkflowConditionInstance;
+
+public class ProteomeFilteringCondition implements WorkflowConditionInstance {
+	
+	private static final Logger LOG = Logger.getLogger(ProteomeFilteringCondition.class.getName());
+
+	@Override
+	public boolean evaluate(Metadata metadata, WorkflowConditionConfiguration config) {
+		
+		for (String key : metadata.getAllKeys()) {
+			LOG.info("==> key: ["+key+"]     value: ["+metadata.getMetadata(key)+"]");
+		}
+		
+		// retrieve job-level metadata
+		List<String> rawFiles = metadata.getAllMetadata("RAWFiles");
+		for (String rawFile : rawFiles) {
+			LOG.info("Raw file="+rawFile);
+		}
+		
+		// retrieve condition configuration
+		String outputDirectory = config.getProperty("outputDirectory");
+		LOG.info("outputDirectory="+outputDirectory);
+
+		// loop over expected output files
+//		Long now = System.currentTimeMillis();
+//		SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
+//		for (int i=1; i<=ncv; i++) {
+//			
+//			String filepath = projectDirectory+"/results/cv/"+prefix+"_iter_"+i+".txt";
+//			File file = new File(filepath);
+//			Long fileLastModified = file.lastModified();
+//			
+//			LOG.info("Checking file: "+filepath+" last modified at:"+sdf.format(fileLastModified));
+//			
+//			// check existence
+//			if (!file.exists()) return false;
+//						
+//		}
+		
+		// all files exist
+		return false;
+	}
+
+}
