9c640c3ba681083117f14f746aabeb3d450a1170
Luca Cinquini <luca.cinquini@jpl.nasa.gov>
2015-03-09 17:52:38 -0600

    Changing the script.

--- backend/workflows/proteome-misc-tasks/src/main/scripts/cystfluid-launch.sh
+++ backend/workflows/proteome-misc-tasks/src/main/scripts/cystfluid-launch.sh
@@ -3,10 +3,11 @@
 --metaData \
 --key RawDatasetName 20140506-Pancreatic-Cyst-Fluid \
 --key PrimaryAnalysisDatasetName 0140506-Pancreatic-Cyst-Fluid-Processed \
---key RAWFileNames A1_frx01.raw A1_frx02.raw A1_frx03.raw \
 --key DBFile /usr/local/edrn/proteome/pipeline-tools/tabblab/protein.db.fasta \
 --key CntmsFile /usr/local/edrn/proteome/pipeline-tools/tabblab/Cntms.fasta \
 --key MyriMatchCfgFile /usr/local/edrn/proteome/pipeline-tools/tabblab/cystfluid.myrimatch.cfg \
 --key AssembleFileList /usr/local/edrn/proteome/pipeline-tools/tabblab/Assembly.txt \
 --key ProteinFile /usr/local/edrn/proteome/pipeline-tools/tabblab/protein.fa \
---key CopyFiles false --key UserEmail luca.cinquini@jpl.nasa.gov
+--key CopyFiles false \
+--key UserEmail luca.cinquini@jpl.nasa.gov
+#--key RAWFileNames A1_frx01.raw A1_frx02.raw A1_frx03.raw
